c407806cbe401ee54c41bb8af339cefb1e6616b3
niahlshanp <nihalshanptm@gamil.com>
2021-07-08 21:12:27 +0530

    learned how to conditionally run effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import ClassCounterOne from "./components/useEffect/ClassCounterOne";
 import CounterOne from "./components/useEffect/CounterOne";
 
 function App() {
   return (
     <div className="App">
       <CounterOne/>
+      {/* <ClassCounterOne/> */}
     </div>
   );
 }
--- src/components/useEffect/CounterOne.js
+++ src/components/useEffect/CounterOne.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 
 const CounterOne = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('')
 
     useEffect(()=> {
+        console.log('useEffect - Updating document title')
         document.title = `You clicked ${count} times`
     })
 
   return (
     <div>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(count + 1)}>Click {count}</button>
     </div>
   );
