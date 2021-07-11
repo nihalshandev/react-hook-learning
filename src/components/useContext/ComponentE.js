41bba9c8619530d13dd104a74c8c4abf45bf902d
niahlshanp <nihalshanptm@gamil.com>
2021-07-11 07:30:53 +0530

    learned working with context api with useContext

--- src/components/useContext/ComponentE.js
+++ src/components/useContext/ComponentE.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ChannelContext, UserContext } from "../../App";
 import ComponentF from "./ComponentF";
 
 const ComponentE = () => {
+    const user = useContext(UserContext)
+    const channel = useContext(ChannelContext)
   return (
     <div>
+        <h4> {user} - {channel} </h4>
+
+        <p>------------------------------</p>
       <ComponentF />
     </div>
   );
